5e114c23a22751b0e96db7ea824f118eb2b244fa
langley <295046974@qq.com>
2017-11-29 13:07:42 +1100

    first commit

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ import traceback
 # Change PUBLIC DNS Name
 # AWS_PUBLIC_DNS = "http://ec2-13-210-137-102.ap-southeast-2.compute.amazonaws.com"  # production environment
 AWS_PUBLIC_DNS = "http://ec2-52-62-225-98.ap-southeast-2.compute.amazonaws.com"  # frank
-ACCESS_ID = 'AKIAIQIVGIH4PJHATQKQ'
-ACCESS_KEY = '/f4hE5Qp5AXiAMwJngx9AOTQDSMPQkChwvIxomM0'
+ACCESS_ID = 'AKIAITPD5GAGJIFXWOPA'
+ACCESS_KEY = '/X2M6oMPofrl3OGDY3FMPG3MxRScsn/cABhKjeSy'
 
 bucket_name = 'ezswitch-image'
 prefix_url = 'https://s3-ap-southeast-2.amazonaws.com'
